e0e7b2ae97cc5b296bc6fb3966d81bffc670df70
md-maruf-hossain <marufhossain.6470@gmail.com>
2024-08-18 15:08:55 +0600

    project created and router added

---
+++ src/Router/Routers.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import ErrorPage from "../Pages/Error/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
