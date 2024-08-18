7421eba5b4fa6583b354d4087dc4c81d9694b1f3
md-maruf-hossain <marufhossain.6470@gmail.com>
2024-08-18 15:50:17 +0600

    navbar added without link

--- src/Pages/Shared/NavBar/Navbar.jsx
+++ src/Pages/Shared/NavBar/Navbar.jsx
@@ -1,10 +1,58 @@
-
 const Navbar = () => {
-    return (
-        <div>
-            <p>navbar</p>
+  return (
+    <div className="navbar">
+      <div className="navbar-start">
+        {/* /navbar drop down for phone */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
         </div>
-    );
+        {/* /navbar drop down for phone */}
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Our Products</a>
+          </li>
+          <li>
+            <a>Why Choose Us</a>
+          </li>
+          <li>
+            <a>Testimonial</a>
+          </li>
+          <li>
+            <a>Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
